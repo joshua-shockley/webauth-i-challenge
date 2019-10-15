26e05ba7e39a847d59d420d5b894b1af8aea36c0
Joshua-Shockley <thelumbashockley@gmail.com>
2019-10-14 22:20:53 -0500

    added the protected middleware with the auth path

---
+++ helperModel/userRouter.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const Users = require('./user-model.js');
+
+const router = express.Router();
+
+
+
+router.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    Users.findById(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    let changes = req.body;
+    Users.findById(id)
+        .then(user => {
+            if (user) {
+                Users.update(changes, id)
+                    .then(updated => {
+                        res.status(201).json(updated);
+                    })
+            } else {
+                res.status(404).json({ message: 'could not find user with given id' });
+            };
+        })
+        .catch(error => {
+            console.log('in catch', id, changes);
+            res.status(500).json({ message: 'failed to update user' });
+        });
+});
+
+router.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    Users.remove(id)
+        .then(gone => {
+
+            res.status(200).json({ message: 'they gone now!' });;
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+
+
+
+module.exports = router;
