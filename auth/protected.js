a5d35b58eac39357a31397ea45d97e86a4f1e5ae
Joshua-Shockley <thelumbashockley@gmail.com>
2019-10-15 14:56:03 -0500

    got the session to work

--- auth/protected.js
+++ auth/protected.js
@@ -1,25 +1,34 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../helperModel/user-model.js');
-
-module.exports = function protected(req, res, next) {
-    const { username, password } = req.headers;
-
-    if (username && password) {
-        Users.findBy({ username })
-            .first()
-            .then(user => {
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    next();
-                    // res.status(200).json({ message: 'now set this to next(); its working' });
-                } else {
-                    res.status(401).json({ message: 'you shall not pass!' });
-                };
-            })
-            .catch(error => {
-                res.status(500).json(error);
-            });
+module.exports = (req, res, next) => {
+    if (req.session && req.session.userId) {
+        next();
     } else {
-        res.status(400).json({ message: 'doesnt see the credentials.. please add some.' });
-    };
-}
+        res.status(401).json({ message: 'you are not logged in yet' });
+    }
+};
+
+
+// const bcrypt = require('bcryptjs');
+
+// const Users = require('../helperModel/user-model.js');
+
+// module.exports = function protected(req, res, next) {
+//     const { username, password } = req.headers;
+
+//     if (username && password) {
+//         Users.findBy({ username })
+//             .first()
+//             .then(user => {
+//                 if (user && bcrypt.compareSync(password, user.password)) {
+//                     next();
+//                     // res.status(200).json({ message: 'now set this to next(); its working' });
+//                 } else {
+//                     res.status(401).json({ message: 'you shall not pass!' });
+//                 };
+//             })
+//             .catch(error => {
+//                 res.status(500).json(error);
+//             });
+//     } else {
+//         res.status(400).json({ message: 'doesnt see the credentials.. please add some.' });
+//     };
+// }
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const session = require('express-session');
+
 const bcrypt = require('bcryptjs');
+
 
 const db = require('./data/dbConfig.js');
 const Users = require('./helperModel/user-model.js');
@@ -10,10 +13,32 @@ const protected = require('./auth/protected.js');
 const UserAuthRouter = require('./helperModel/userRouter.js');
 const server = express();
 
+const sessionConfig = {
+    name: 'turtle',
+    secret: 'i am the one and only secret',
+    cookie: {
+        maxAge: 1000 * 60 * 60, //this is how long the cookie is good for. 1000=1sec *60 makes it a minute the *60 makes it 60minutes.
+        secure: false,
+        httpOnly: true, //remember this prevents using javascript to access it.
+    },
+    resave: false, // do not recreate session when changed?
+    saveUninitialized: true, // prevents (when false in production) from creating cookies automatically without client permission it a legal thing look up GDPR laws
+    //this is where the store goes if gonna use it.
+    //store: new knexSessionStore({
+    //     knex: knexConfig,//need to make this above
+    //     createtable: true,//needs to be in lowercase for some odd reason
+    //     clearInterval: 100 * 60 * 30,// same for time expiration as above for maxAge.. this clears the database of the unused cookies.    
+    // })
+};
+
+
+//global middleware
 server.use(logger);
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
+
 
 server.use('/api/auth/', protected, UserAuthRouter);
 
@@ -72,7 +97,9 @@ server.post('/api/login', (req, res) => {
             .first()
             .then(user => {
                 if (user && bcrypt.compareSync(password, user.password)) {
+                    req.session.userId = user.id;
                     res.status(200).json({ message: `${user.username} is now logged in` });
+                    console.log(req.session);
 
                 } else {
                     res.status(401).json({ message: 'credentials invalid' })
@@ -83,6 +110,20 @@ server.post('/api/login', (req, res) => {
             });
     } else {
         res.status(500).json({ message: 'apply credentials please' });
+    };
+});
+
+server.get('/api/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.json({ message: 'you did not logout yet.. try again...lol' });
+            } else {
+                res.status(200).json({ message: 'successful logout, see ya later' });
+            };
+        })
+    } else {
+        res.status(200).json({ message: 'thats funny... you were never logged in' });
     };
 });
 
